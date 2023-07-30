24d8180773c87eec86ee3833e5691ae6fb8430d5
Hackman.Lo <admin@yytec.net>
2023-07-30 20:58:59 +0800

    Initialize

---
+++ database/migrations/2023_07_30_115339_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_article', function (Blueprint $table) {
+            $table->id("article_id");
+            $table->text("title");
+            $table->longText("content");
+            $table->foreignId("fn_author_id");
+            $table->unsignedInteger("order")->default(0);
+            $table->json("keywords")->default('');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_article');
+    }
+};
---
+++ database/migrations/2023_07_30_122241_create_attachments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_attachment', function (Blueprint $table) {
+            $table->id("attachment_id");
+            $table->foreignId("fn_owner_id");
+            $table->string("url", 255);
+            $table->string("access_url", 255);
+            $table->string("thumbnail_url", 255);
+            $table->string("original_name", 255);
+            $table->unsignedBigInteger("file_size")->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_attachment');
+    }
+};
