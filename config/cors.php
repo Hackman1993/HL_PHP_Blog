8184aad157ed4bb198c644485e3cd09a43eed23f
hackman <admin@yytec.net>
2023-10-16 15:27:18 +0800

    Rendering Support

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ["*"],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://test1.i4u.icu', 'http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5173', 'http://www.zk-tec.cn', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
